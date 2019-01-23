41a381ccfe0fe6cf12c25da567dc83ec1a7f6846
Unknown <kralc1975@gmail.com>
2019-01-23 10:26:35 -0700

    Started Project

---
+++ Adventure game?/game.js
@@ -0,0 +1,20 @@
+// Comment
+/*
+
+Comment
+
+*/
+
+//document.write("<h1> I can't wait for <em>spring!</em> </h1>");
+
+
+//alert("I'm stealing your vbucks, k?");
+
+//cofirm("Do you like explosives?");
+
+//prompt("What type of explosives do you like?");
+
+var pc = prompt("What is your name?");
+
+confirm("So your name is "+ pc +"?");
+
