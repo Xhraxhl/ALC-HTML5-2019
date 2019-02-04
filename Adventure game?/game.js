39afb0061a45a75731a0c02ff4d786401a780dd1
Unknown <kralc1975@gmail.com>
2019-02-04 10:08:44 -0700

    Trying to finish an area
    This is going WAYYYY slow

--- Adventure game?/game.js
+++ Adventure game?/game.js
@@ -14,67 +14,163 @@ Comment
 
 //prompt("What type of cars do you like?");
 
+var inventory = {
+    coins:100,
+    armor:0,
+    weapon:0,
+    food:0,
+}
+var pcStats = {
+    life:100,
+    attack:0,
+    armor_rating:0,
+}
+var enemy = {
+    life:50,
+    attack:10,
+    armor_rating:0,
+}
 
 Game();
 
 function Game(){
     
-    document.write("This game is a WIP, it is imcomplete, so please be understanding if there are areas or commands that do not work. \n - Clark Allen");
+    document.write("This game is a WIP, it is imcomplete, so please be understanding if there are areas or commands that do not work.");
+    alert("At the moment the Castle area is the only area that is coded, so go in the other directions at your own risk.")
     var playerName = prompt("What is your name?");
     alert("Welcome to the game "+playerName);
     
     Crossroads();
     
     function Crossroads(){
-        var crossroads = prompt("You wake up in the middle of an intersection, with no idea how you got there. Unable to see through your blurred eyes you wait until your vision clears. \n -Look around \n - Walk in a direction N,S,E,W").toLowerCase();
+        var crossRoads = prompt("You wake up in the middle of an intersection, with no idea how you got there. Unable to see through your blurred eyes you wait until your vision clears. \n -Look around \n - Walk in a direction N,S,E,W").toLowerCase();
 
         CrossRLook();
         
         function CrossRLook(){
-            if( crossroads == "look" || crossroads == "look around"){
+            if( crossRoads == "look" || crossRoads == "look around"){
                 //document.write(".\n The stuff works ok?")
-                var crossroadslook = prompt("You notice there is a note beside you, a sign in the middle of the road, and in the distance there is a familiar city to the North, a flying city to the East, a land of ruins to the South, and a castle to the West. \n Read note \n Read sign \n Crossroad").toLowerCase();
+                var crossRoadsLook = prompt("You notice there is a note beside you, a sign in the middle of the road, and in the distance there is a familiar city to the North, a flying city to the East, a land of ruins to the South, and a castle to the West. \n Read note \n Read sign \n Crossroad").toLowerCase();
              
-                if( crossroadslook == "read note"){
+                if( crossRoadsLook == "read note"){
                     alert("You did this to yourself "+playerName+" enjoy your new life. And don't bother trying to come back, you can't. \n - X");
                     CrossRLook();
                 }
-                else if( crossroadslook == "read sign"){
-                    var crosssign = prompt("Welcome to x type help to get a command list, you can only access this list from this sign. Or type exit to leave the game. To Resume type exit then hit cancel.").toLowerCase();
-                    if( crosssign == "help" || crosssign == "h"){
+                else if( crossRoadsLook == "read sign"){
+                    var crossSign = prompt("Welcome to x type help to get a command list, you can only access this list from this sign. Or type exit to leave the game. To Resume type exit then hit cancel.").toLowerCase();
+                    if( crossSign == "help" || crossSign == "h"){
                         alert("command list");
                         CrossRLook();
                     }
-                    else if( crosssign == "exit"){
-                        var exitgame = confirm("Are you sure you wish to exit?");
-                        if( exitgame){
+                    else if( crossSign == "exit"){
+                        var exitGame = confirm("Are you sure you wish to exit?");
+                        if( exitGame){
                             alert(playerName+" has left the game.");
                         }
                         else{
                             CrossRLook();
                         }
                     }
-                    else if( crossroadslook == "crossroad"){
+                    else if( crossRoadsLook == "crossroad"){
                         Crossroads();
                     }
-
                 }
             }
-            else if( crossroads == "walk n" || crossroads == "n"){
+            else if( crossRoads == "walk n" || crossRoads == "n"){
                 Nroad1();
+            }
+            else if( crossRoads == "walk w" || crossRoads == "w"){
+                Wroad1();
+            }
+            else if( crossRoads == "walk s" || crossRoads == "s"){
+                alert("Incomplete area.");
+                CrossRLook();
+            }
+            else if( crossRoads == "walk e" || crossRoads == "e"){
+                alert("Incomplete area.");
+                CrossRLook();
             }
             else{
                 alert("Aight, type a command thats given.");
                 CrossRLook();
             }
-        }
-    
+        } 
     }
     function Nroad1(){
-        var roadn1 = prompt("You walk along the road until you are tired and need to rest. The City looks closer than it used to. \n N,S,E,W \n Look around").toLowerCase();
-        if( roadn1 == "look" || roadn1 == "look around"){
-            var roadn1Look = prompt("The City is in the distance, the crossroads are behind you. Though farther away now you can still see the castle to the West, the flying city to the East, and the ruins to the South.")
+        var roadN1 = prompt("You walk along the road until you are tired and need to rest. The City looks closer than it used to. \n N,S,E,W \n Look around").toLowerCase();
+        if( roadN1 == "look" || roadN1 == "look around"){
+            var roadn1Look = prompt("The City is in the distance, the crossroads are behind you. Though farther away now you can still see the castle to the West, the flying city to the East, and the ruins to the South.  \n N,S,E,W \n Look around")
         }
+    }
+    function Wroad1(){
+        var roadW1 = prompt("You walk along the road for a while and then sit down to rest. The Castle looks closer than it used to. \n N,S,E,W \n Look around").toLowerCase();
+        if( roadW1 == "look" || roadW1 == "look around"){
+            var roadW1Look = prompt("The Castle is closer than before. You can still see the Ruins and the City, the Flying City is faded into the distance.  \n N,S,E,W").toLowerCase();
+            if(roadW1Look == "n" || roadW1Look == "walk n"){
+                alert("Incomplete area");
+                Wroad1();
+            }
+            else if(roadW1Look == "w" || roadW1Look == "walk w"){
+                Wroad2();
+            }
+            else if(roadW1Look == "s" || roadW1Look == "walk s"){
+                alert("Incomplete area");
+                Wroad1();
+            }
+            else if(roadW1Look == "e" || roadW1Look == "walk e"){
+                alert("Incomplete area");
+                Wroad1();
+            }
+            else{
+                alert("Please use a suggested command.");
+                Wroad1();
+            }
+        }
+        else if(roadW1 == "n" || roadW1 == "walk n"){
+            alert("Incomplete area");
+            Wroad1();
+        }
+        else if(roadW1 == "w" || roadW1 == "walk w"){
+            Wroad2();
+        }
+        else if(roadW1 == "s" || roadW1 == "walk s"){
+            alert("Incomplete area");
+            Wroad1();
+        }
+        else if(roadW1 == "e" || roadW1 == "walk e"){
+            alert("Incomplete area");
+            Wroad1();
+        }
+        else{
+            alert("Please use a suggested command.");
+            Wroad1();
+        }
+    }
+    function Wroad2(){
+        var roadW2 = prompt("You resume walking for a while. \n N,S,E,W \n Look around");
+        if(roadW2 == "look" || roadW2 == "look around"){
+            var roadW2Look = prompt("You can now barely see the flying city in the distance, the castle is now huge, and the city and ruins are farther away than before.");
+        }
+    }
+    function Wroad3(){
+
+    }
+    function KeepSmith(){
+        alert("The Blacksmith looks over at you, scowls and the slowly walks over to see what you want.");
+            var keepSmith = prompt("What would you like to buy? \n Bow \n Armor \n Leave").toLowerCase();
+                if( keepSmith == "weapon"){
+                    var weaponBuy = confirm("Are you sure you want to buy this Bow?");
+                        if(weaponBuy){
+                            inventory.weapon ++;
+                            alert("You now have "+inventory.weapon+" weapons!");
+                            inventory.coins = inventory.coins - 10;
+                            alert("You now have "+inventory.coins+" coins!");
+                            KeepSmith();
+                        }
+                        else{
+                            KeepSmith();
+                        }
+                }
     }
 
 }
